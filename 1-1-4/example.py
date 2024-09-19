9372d6f598ec618c55532b8979e920a11acd9c22
Jack <jack.turner@vtsd.com>
2024-09-19 11:46:23 -0400

    New soultion step 18 in 1.1.3

---
+++ 1-1-4/example.py
@@ -0,0 +1,10 @@
+num1 = int(input("please enter a number"))
+num2 = int(input("please enter a number"))
+
+if num1 > num2:
+    print("number 1 is bigger")
+elif num2 > num1:
+    print("number 2 is bigger")
+else:
+    print("number 1 and number 2 are equal")
+
---
+++ 1-1-4/grades.py
@@ -0,0 +1,5 @@
+grade = int(input("Please enter the grade you got on your last test"))
+if grade > 90:
+    print("A")
+elif grade > 80:
+    print("B")
