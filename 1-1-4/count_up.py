47ad049a140ee03b2ba41cead30659966d49ee3b
Jack <jack.turner@vtsd.com>
2024-09-24 12:29:03 -0400

    New soultion step 18 in 1.1.3

---
+++ 1-1-4/count_up.py
@@ -0,0 +1,12 @@
+# Marcus fave number, thus why is  in this example
+#Gio fave number was 852
+
+total = 0
+increment = 13
+goal= 1042
+while total + increment <= goal:
+# ADD 11 TO MY TOTAL
+    total += increment
+    print(total)
+
+# tell the user the final number
---
+++ 1-1-4/loops.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a number"))
+num2 = int(input("Enter another number (all the math!!!)"))
+
+while num1 != num2:
+    print("You didnt enter the same number")
+    print("Please enter a valid password ")
+    num1 = int(input("Enter a number"))
+    num2 = int(input("Enter another number"))
+
+print("you got it")
