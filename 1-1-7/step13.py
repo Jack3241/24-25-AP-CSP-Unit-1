0e3b353c04ed76c00076d5f13af35cab4f6440fc
Jack <jack.turner@vtsd.com>
2024-10-08 13:45:52 -0400

    1.1.7 steps 14 to end

--- 1-1-7/step13.py
+++ 1-1-7/step13.py
@@ -9,29 +9,29 @@ my_turtles = []
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
-index = 0
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   t.color(turtle_colors.pop())
-  index += 1
   t.penup()
   my_turtles.append(t)
 
 #
 startx = 0
 starty = 0
-
+direction = 90
 #
 for t in my_turtles:
   t.goto(startx, starty)
+  t.setheading(direction)
   t.pendown()
   t.right(45)
   t.forward(50)
-
+  direction = t.heading()
 #
-  startx = startx + 50
-  starty = starty + 50
+  startx = t.xcor()
+  starty = t.ycor()
+
 
 wn = trtl.Screen()
 wn.mainloop()
