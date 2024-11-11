4c3d0ad0b2f76b098e5c7cf578c8158bda606dd9
Jack <jack.turner@vtsd.com>
2024-11-11 13:08:48 -0500

    1.2.2 18-21

--- 1-2-2/leaderboard.py
+++ 1-2-2/leaderboard.py
@@ -22,8 +22,9 @@ def get_names(file_name):
         while line[index] != ",":
             leader_name = leader_name + line[index]
             index = index + 1
+        print("leader name is:", leader_name)
         # TODO 2: add the player name to the names list
-
+        names.append(leader_name)
     leaderboard_file.close()
 
     #  TODO 6: return the names list in place of the empty list
